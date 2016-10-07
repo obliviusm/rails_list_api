8a5aa335f3de3d39b9f2e9dc24df9d8956458196
obliviusm <obliviusm@gmail.com>
2016-10-08 01:21:19 +0300

    add seeds for project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  Project.create(
+    title: Faker::Company.name, 
+    image: Faker::Company.logo,
+    headquarters: Faker::Address.city,
+    founded_at: Faker::Date.between(10.years.ago, 1.year.ago),
+    founders: Faker::StarWars.character,
+    category: Faker::Book.genre
+  )
+end
