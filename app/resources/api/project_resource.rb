80e9a221a4bff1c453d4eb1b8f8b188473bf6077
obliviusm <obliviusm@gmail.com>
2016-10-09 03:03:23 +0300

    add filter

--- app/resources/api/project_resource.rb
+++ app/resources/api/project_resource.rb
@@ -1,5 +1,8 @@
 module Api
   class ProjectResource < JSONAPI::Resource
     attributes :title, :founders, :headquarters, :category, :founded_at, :image
+    filter :category, apply: ->(records, value, _options) {
+      records.where("category LIKE LOWER(?)", "%#{value.first.downcase}%")
+    }
   end
 end
