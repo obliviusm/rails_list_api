4524203a2dbd2b4bf49627ae6dc33d0eb20a9057
obliviusm <obliviusm@gmail.com>
2016-10-10 16:25:17 +0300

    make filter case insensitive

--- app/resources/api/project_resource.rb
+++ app/resources/api/project_resource.rb
@@ -2,7 +2,7 @@ module Api
   class ProjectResource < JSONAPI::Resource
     attributes :title, :founders, :headquarters, :category, :founded_at, :image
     filter :category, apply: ->(records, value, _options) {
-      records.where("category LIKE LOWER(?)", "%#{value.first.downcase}%")
+      records.where("category ILIKE ?", "%#{value.first}%")
     }
   end
 end
